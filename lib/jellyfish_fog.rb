fc3671608051b95f93d13c8af1a34c13fa0e571a
mafernando <fernando_michael@bah.com>
2015-05-11 21:51:26 -0500

    split JellyfishFog into Jellyfish::Fog

--- lib/jellyfish_fog.rb
+++ lib/jellyfish_fog.rb
@@ -1,4 +1,20 @@
+require 'fog'
+require 'bcrypt'
 require 'jellyfish_fog/engine'
+require 'jellyfish_fog/provisioner'
+require 'jellyfish_fog/infrastructure'
+require 'jellyfish_fog/databases'
+require 'jellyfish_fog/storage'
 
-module JellyfishFog
+module Jellyfish
+  module Fog
+    module AWS
+      def self.aws_settings
+        {
+            aws_access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+            aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
+        }
+      end
+    end
+  end
 end
