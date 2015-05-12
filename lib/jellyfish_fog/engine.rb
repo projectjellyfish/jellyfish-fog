86af1c78f744d3ab7badf5eca39c6375c55d88fe
mafernando <fernando_michael@bah.com>
2015-05-11 21:50:52 -0500

    split JellyfishFog into Jellyfish::Fog

--- lib/jellyfish_fog/engine.rb
+++ lib/jellyfish_fog/engine.rb
@@ -1,9 +1,11 @@
-module JellyfishFog
-  class Engine < ::Rails::Engine
-    isolate_namespace JellyfishFog
-    config.autoload_paths += %W(#{config.root}/lib)
-    config.generators do |g|
-      g.test_framework :rspec
+module Jellyfish
+  module Fog
+    class Engine < ::Rails::Engine
+      isolate_namespace Jellyfish::Fog
+      config.autoload_paths += %W(#{config.root}/lib)
+      config.generators do |g|
+        g.test_framework :rspec
+      end
     end
   end
 end
