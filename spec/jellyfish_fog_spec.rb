b6772658e20e476efe6df7d904c8a73c23cf85c5
mafernando <fernando_michael@bah.com>
2015-05-12 11:48:08 -0500

    add specs back into repo which can be run with rake

---
+++ spec/jellyfish_fog_spec.rb
@@ -0,0 +1,11 @@
+# require 'spec_helper'
+
+module Jellyfish
+  module Fog
+    describe 'Registering product types' do
+      it 'registers all product types' do
+        expect(Dummy::Application.config.x.product_types).to have_key('AWS Fog Databases')
+      end
+    end
+  end
+end
