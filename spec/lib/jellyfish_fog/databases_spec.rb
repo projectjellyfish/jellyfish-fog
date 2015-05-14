4bc51e346abf4e0dfbf8ad1e36bf85033cd6eb76
mafernando <fernando_michael@bah.com>
2015-05-14 11:56:57 -0500

    move aws functionality back into aws module so we can create modules for azure, digitialocean and vmware later

--- spec/lib/jellyfish_fog/databases_spec.rb
+++ spec/lib/jellyfish_fog/databases_spec.rb
@@ -1,40 +1,41 @@
 module Jellyfish
   module Fog
-    describe Databases do
-      it 'provisions and retires databases using fog' do
-        enable_aws_fog_provisioning
+    module AWS
+      describe Databases do
+        it 'provisions and retires databases using fog' do
+          enable_aws_fog_provisioning
 
-        Databases.new(order_item).provision
+          Databases.new(order_item).provision
 
-        expect(order_item.provision_status).to eq 'ok'
-        order_item.payload_response = JSON.parse(order_item.payload_response)
-        expect(
-          order_item.payload_response['data']['body']['CreateDBInstanceResult']['DBInstance']
-        ).to include(
-          order_item.answers.merge('DBInstanceIdentifier' => "id-#{order_item.uuid}")
-        )
+          expect(order_item.provision_status).to eq 'ok'
+          order_item.payload_response = JSON.parse(order_item.payload_response)
+          expect(
+            order_item.payload_response['data']['body']['CreateDBInstanceResult']['DBInstance']
+          ).to include(
+            order_item.answers.merge('DBInstanceIdentifier' => "id-#{order_item.uuid}")
+          )
 
-        Databases.new(order_item).retire
+          Databases.new(order_item).retire
 
-        expect(order_item.provision_status).to eq 'retired'
-      end
-
-
-      def order_item
-        @order_item ||= OpenStruct.new.tap do |order_item|
-          order_item.uuid = '1234567890'
-          order_item.answers = {
-            'AllocatedStorage' => 100,
-            'DBInstanceClass' => 'Test',
-            'Engine' => 'Test'
-          }
+          expect(order_item.provision_status).to eq 'retired'
         end
-      end
 
-      def enable_aws_fog_provisioning
-        ::Fog.mock!
-        allow(ENV).to receive(:fetch).with('AWS_ACCESS_KEY_ID').and_return('text')
-        allow(ENV).to receive(:fetch).with('AWS_SECRET_ACCESS_KEY').and_return('text')
+        def order_item
+          @order_item ||= OpenStruct.new.tap do |order_item|
+            order_item.uuid = '1234567890'
+            order_item.answers = {
+              'AllocatedStorage' => 100,
+              'DBInstanceClass' => 'Test',
+              'Engine' => 'Test'
+            }
+          end
+        end
+
+        def enable_aws_fog_provisioning
+          ::Fog.mock!
+          allow(ENV).to receive(:fetch).with('AWS_ACCESS_KEY_ID').and_return('text')
+          allow(ENV).to receive(:fetch).with('AWS_SECRET_ACCESS_KEY').and_return('text')
+        end
       end
     end
   end
--- spec/lib/jellyfish_fog/infrastructure_spec.rb
+++ spec/lib/jellyfish_fog/infrastructure_spec.rb
@@ -1,29 +1,31 @@
 module Jellyfish
   module Fog
-    describe Infrastructure do
-      it 'provisions and retires infrastructure using fog' do
-        enable_aws_fog_provisioning
+    module AWS
+      describe Infrastructure do
+        it 'provisions and retires infrastructure using fog' do
+          enable_aws_fog_provisioning
 
-        Infrastructure.new(order_item).provision
+          Infrastructure.new(order_item).provision
 
-        expect(order_item.provision_status).to eq 'ok'
-        order_item.payload_response = JSON.parse(order_item.payload_response)
-        expect(order_item.payload_response).to have_key('id')
-        expect(order_item.payload_response['image_id']).to eq('ami-1ccae774')
+          expect(order_item.provision_status).to eq 'ok'
+          order_item.payload_response = JSON.parse(order_item.payload_response)
+          expect(order_item.payload_response).to have_key('id')
+          expect(order_item.payload_response['image_id']).to eq('ami-1ccae774')
 
-        Infrastructure.new(order_item).retire
+          Infrastructure.new(order_item).retire
 
-        expect(order_item.provision_status).to eq 'retired'
-      end
+          expect(order_item.provision_status).to eq 'retired'
+        end
 
-      def order_item
-        @order_item ||= OpenStruct.new
-      end
+        def order_item
+          @order_item ||= OpenStruct.new
+        end
 
-      def enable_aws_fog_provisioning
-        ::Fog.mock!
-        allow(ENV).to receive(:fetch).with('AWS_ACCESS_KEY_ID').and_return('text')
-        allow(ENV).to receive(:fetch).with('AWS_SECRET_ACCESS_KEY').and_return('text')
+        def enable_aws_fog_provisioning
+          ::Fog.mock!
+          allow(ENV).to receive(:fetch).with('AWS_ACCESS_KEY_ID').and_return('text')
+          allow(ENV).to receive(:fetch).with('AWS_SECRET_ACCESS_KEY').and_return('text')
+        end
       end
     end
   end
