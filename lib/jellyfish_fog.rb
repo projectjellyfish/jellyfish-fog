11607d13ce3ec3e8f284df9c640e6a40bcb46dd7
mafernando <fernando_michael@bah.com>
2015-05-14 11:50:45 -0500

    correct settings indention

--- lib/jellyfish_fog.rb
+++ lib/jellyfish_fog.rb
@@ -8,11 +8,11 @@ require 'jellyfish_fog/storage'
 
 module Jellyfish
   module Fog
-      def self.aws_settings
-        {
-            aws_access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-            aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
-        }
-      end
+    def self.aws_settings
+      {
+        aws_access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
+      }
+    end
   end
 end
