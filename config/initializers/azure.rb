9c024d9e01ec6a113fa5f3e51c3ed1c13ee6fe16
mafernando <fernando_michael@bah.com>
2015-05-29 15:44:42 -0500

    corrected how azure initializer loads env vars

--- config/initializers/azure.rb
+++ config/initializers/azure.rb
@@ -2,7 +2,7 @@ require 'azure'
 
 Azure.configure do |config|
   # Configure these 3 properties to use Storage
-  config.management_certificate = ENV.fetch('JELLYFISH_AZURE_PEM_PATH')
-  config.subscription_id        = ENV.fetch('JELLYFISH_AZURE_SUB_ID')
-  config.management_endpoint    = ENV.fetch('JELLYFISH_AZURE_API_URL')
+  config.management_certificate = ENV['JELLYFISH_AZURE_PEM_PATH']
+  config.subscription_id        = ENV['JELLYFISH_AZURE_SUB_ID']
+  config.management_endpoint    = ENV['JELLYFISH_AZURE_API_URL']
 end
