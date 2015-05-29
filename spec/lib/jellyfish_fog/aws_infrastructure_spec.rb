64e49074c379eab86ebd379874b2fe17fe19853a
mafernando <fernando_michael@bah.com>
2015-05-29 15:44:05 -0500

    renamed aws specific specs with prefixes

--- spec/lib/jellyfish_fog/aws_infrastructure_spec.rb
+++ spec/lib/jellyfish_fog/aws_infrastructure_spec.rb
@@ -8,14 +8,14 @@ module Jellyfish
           order_item.answers = { 'image_id' => 'ami-1ccae774' }
           Infrastructure.new(order_item).provision
 
-          expect(order_item.provision_status).to eq 'ok'
+          expect(order_item.provision_status).to eq :ok
           order_item.payload_response = JSON.parse(order_item.payload_response)
           expect(order_item.payload_response).to have_key('id')
           expect(order_item.payload_response['image_id']).to eq('ami-1ccae774')
 
           Infrastructure.new(order_item).retire
 
-          expect(order_item.provision_status).to eq 'retired'
+          expect(order_item.provision_status).to eq :retired
         end
 
         def order_item
